e42b328ff056db759660e1b22f5d27906aac5f47
Andrew Walter <awalter@bnl.gov>
2019-02-06 13:54:09 -0500

    FIX: a minor change to ensure it works with the latest commmit to suitcase.utils

--- suitcase/json_metadata/conftest.py
+++ suitcase/json_metadata/conftest.py
@@ -1,5 +1,5 @@
 from bluesky.tests.conftest import RE  # noqa
 from ophyd.tests.conftest import hw  # noqa
-from suitcase.utils.conftest import (example_data, generate_data,
-                                     detector_list, event_type,
-                                     plan_type)  # noqa
+from suitcase.utils.tests.conftest import (example_data, generate_data,
+                                           detector_list, event_type,
+                                           plan_type)  # noqa
