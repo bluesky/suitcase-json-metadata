01550d0e2341b8f115088be1ed878987612abc37
Andrew Walter <awalter@bnl.gov>
2019-02-11 09:07:20 -0500

    ENH: use `event_model.sanitize_docs` in tests.py

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# suitcase.json_metadata
+# suitcase.json-metadata
 
 This is a suitcase subpackage for reading a particular file format.
 
 ## Installation
 
 ```
-pip install suitcase-json_metadata
+pip install suitcase-json-metadata
 ```
--- suitcase/json_metadata/tests.py
+++ suitcase/json_metadata/tests.py
@@ -7,21 +7,11 @@ import json
 def create_expected(collector):
     '''This collects the metadata into a dict to compare to the loaded data
     '''
-
-    def _tuples_to_lists(val):
-        ''' Recursively convert tuples to lists'''
-        if type(val) in [tuple, list]:
-            items = []
-            for item in val:
-                items.append(_tuples_to_lists(item))
-            val = items
-        return val
-
     expected = {'metadata': {}}
     expected['metadata']['descriptors'] = defaultdict(dict)
     for name, doc in collector:
         if name in ['start', 'stop']:
-            sanitized_doc = _apply_to_dict_recursively(doc, _tuples_to_lists)
+            sanitized_doc = event_model.sanitize_doc(doc)
             expected['metadata'][name] = doc
         elif name == 'descriptor':
             sanitized_doc = event_model.sanitize_doc(doc)
@@ -29,24 +19,6 @@ def create_expected(collector):
                                  ][doc.get('name')][doc['uid']] = sanitized_doc
 
     return expected
-
-
-def _apply_to_dict_recursively(dictionary, func):
-    '''Recursively and apply a function to a dictionary of dictionaries.
-    Takes in a (potentially nested) dictionary and applies a function to each
-    value in the dictionary
-    Parameters
-    ----------
-    dictionary : dict
-        The (potentially nested) dictionary to be recursed.
-    func : function
-        A function to apply to each value in dictionary.
-    '''
-
-    for key, val in dictionary.items():
-        if hasattr(val, 'items'):
-            dictionary[key] = _apply_to_dict_recursively(val, func)
-        dictionary[key] = func(val)
 
 
 def test_export(tmp_path, example_data):
