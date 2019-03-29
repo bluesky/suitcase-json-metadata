9751a6787b37181f52cc177c9496ebb696e3e58e
Maksim Rakitin <mrakitin@bnl.gov>
2019-03-29 13:00:36 -0400

    TST: add a test for kwargs in exporter

--- suitcase/json_metadata/tests/tests.py
+++ suitcase/json_metadata/tests/tests.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import event_model
 from .. import export
 import json
+import pytest
 
 
 def create_expected(collector):
@@ -35,6 +36,30 @@ def test_export(tmp_path, example_data):
     collector = example_data()
     expected = create_expected(collector)
     artifacts = export(collector, tmp_path, file_prefix='')
+
+    for filename in artifacts['run_metadata']:
+        with open(filename) as f:
+            actual = json.load(f)
+        assert actual == expected
+
+
+@pytest.mark.parametrize("kwargs", [{'sort_keys': True, 'indent': 4},
+                                    {'sort_keys': True, 'indent': 2,
+                                     'separators': (',', ':')}])
+def test_export_with_kwargs(tmp_path, example_data, kwargs):
+    ''' runs a test using the plan that is passed through to it
+
+    ..note::
+
+        Due to the `events_data` `pytest.fixture` this will run multiple tests
+        each with a range of detectors and a range of event_types. see
+        `suitcase.utils.conftest` for more info
+
+    '''
+
+    collector = example_data()
+    expected = create_expected(collector)
+    artifacts = export(collector, tmp_path, file_prefix='', **kwargs)
 
     for filename in artifacts['run_metadata']:
         with open(filename) as f:
